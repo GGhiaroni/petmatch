0856eaf944f99d6ee3cdd223a8f2e060f13f4c62
GGhiaroni <gghiaronitiziano@gmail.com>
2025-03-30 13:12:25 -0300

    comecando a criar o input

--- pages/PaginaEncontrar.jsx
+++ pages/PaginaEncontrar.jsx
@@ -1,15 +1,63 @@
+import { CiSearch } from "react-icons/ci";
 import styled from "styled-components";
 
-const Texto = styled.h1`
+const ContainerPrincipal = styled.div`
+  background: #ffffff;
+  min-height: 100vh;
+  width: 100%;
+`;
+
+const TextoPrincipal = styled.h1`
+  font-size: 2.5rem;
+  text-align: center;
+`;
+
+const ContainerTopo = styled.div`
+  padding-top: 9rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2.5rem;
+`;
+
+const ContainerInput = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  background: transparent;
+  border-radius: 10px;
+  padding: 10px 15px;
+  width: 500px;
+  border: 1px solid #d4d8de;
+`;
+
+const InputEstilizado = styled.input`
+  border: none;
+  background: transparent;
+  outline: none;
   font-size: 1rem;
-  margin-top: 15rem;
+  flex: 1;
+
+  ::placeholder {
+    color: #d4d8de;
+  }
 `;
 
 const PaginaEncontrar = () => {
   return (
-    <>
-      <Texto>pagina encontrar</Texto>
-    </>
+    <ContainerPrincipal>
+      <ContainerTopo>
+        <TextoPrincipal>Encontre seu novo amigo</TextoPrincipal>
+        <ContainerInput>
+          <CiSearch size={20} color="#d4d8dE" />
+          <InputEstilizado
+            type="text"
+            placeholder="Buscar por raça ou porte..."
+            color=""
+          />
+        </ContainerInput>
+      </ContainerTopo>
+    </ContainerPrincipal>
   );
 };
 
