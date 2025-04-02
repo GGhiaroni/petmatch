e5328af4c74c50bbb167b250b0d835934c924d9a
GGhiaroni <gghiaronitiziano@gmail.com>
2025-04-02 13:17:59 -0300

    comecando a criar a página única de cada cachorro

--- pages/PaginaDetalhesCachorro.jsx
+++ pages/PaginaDetalhesCachorro.jsx
@@ -1,9 +1,23 @@
+import { LuArrowLeft } from "react-icons/lu";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import listaInicialCachorros from "../src/db/dogs.json";
 
-const H1 = styled.h1`
-  margin-top: 20rem;
+const ContainerPrincipal = styled.div`
+  background: #ffffff;
+  min-height: 100vh;
+  width: 100%;
+  overflow-x: hidden;
+  padding: 8rem 2rem;
+`;
+
+const H3Estilizado = styled.h3`
+  font-size: 1.1rem;
+  color: #3c83f6;
+  font-weight: 300;
+  display: flex;
+  align-items: center;
+  gap: 8px;
 `;
 
 const PaginaDetalhesCachorro = () => {
@@ -12,7 +26,14 @@ const PaginaDetalhesCachorro = () => {
     (cachorro) => cachorro.id === Number(parametroUrl.id)
   );
 
-  return <H1>{cachorro.raca}</H1>;
+  return (
+    <ContainerPrincipal>
+      <H3Estilizado>
+        <LuArrowLeft />
+        Voltar para lista
+      </H3Estilizado>
+    </ContainerPrincipal>
+  );
 };
 
 export default PaginaDetalhesCachorro;
